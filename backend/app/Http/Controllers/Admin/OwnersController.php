e7b85fb39a51ecf98bb3a2f9c6904a22392bcbe1
billiejoenahema <billiejoenahema@gmail.com>
2021-09-15 11:32:11 +0900

    implement: owners store

--- backend/app/Http/Controllers/Admin/OwnersController.php
+++ backend/app/Http/Controllers/Admin/OwnersController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Owner; // Eloquent
-use Illuminate\Support\Facades\DB; // QueryBuilder
-use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
 
 class OwnersController extends Controller
 {
@@ -48,7 +48,19 @@ class OwnersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:owners'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        Owner::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()->route('admin.owners.index');
     }
 
     /**
