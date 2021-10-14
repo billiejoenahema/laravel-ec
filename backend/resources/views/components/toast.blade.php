abbb33527f763c3cf761de12108ce6d87b0ed27d
billiejoenahema <billiejoenahema@gmail.com>
2021-10-15 08:07:11 +0900

    chore: format code

--- backend/resources/views/components/toast.blade.php
+++ backend/resources/views/components/toast.blade.php
@@ -5,7 +5,6 @@
     <div class="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md">
         <div class="flex items-center justify-center w-4 bg-{{$color}}">
         </div>
-
         <div class="px-4 py-2 -mx-3">
             <div class="mx-3">
                 <span class="font-semibold text-{{$color}}">{{session('message')}}
